521d3545090a1e2177a89a8a81a6de0564d6653f
Darth-Revan-13 <alexander.hilbert@bryanuniversity.edu>
2020-07-12 13:05:58 -0400

    js update

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const goombaCost = 5;
 const bobombCost = 7;
 const cheepCheepCost = 11;
 const totalLine = document.getElementById(total);
+const coinForm = document.pricing;
+
+coinForm.addEventListener("submit", cost);
 
 function cost() {
     var goombaTotal = goomba * goombaCost;
     var bobombTotal = bobomb * bobombCost;
     var cheepCheepTotal = cheepCheep * cheepCheepCost;
-
     var totalCost = goombaTotal + bobombTotal + cheepCheepTotal;
     var totalText = document.createElement("p");
     totalText.textContent = totalCost;
